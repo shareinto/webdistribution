30b15fc91e3212c88d06f8bac4e4676233596055
unknown <shareinto>
2015-08-07 20:05:38 +0800

    modified:   src/main/java/com/omco/ci/controller/ManageController.java

--- src/main/java/com/omco/ci/controller/ManageController.java
+++ src/main/java/com/omco/ci/controller/ManageController.java
@@ -1,4 +1,4 @@
-﻿package com.omco.ci.controller;
+package com.omco.ci.controller;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -27,7 +27,7 @@ public class ManageController {
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test()
 	{
-		return "hello docker!thi is a test";
+		return "hello docker!";
 	}
 	
 	// µ¥ÎÄ¼þÉÏ´«
